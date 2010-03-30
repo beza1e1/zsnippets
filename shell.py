759f93b6dba01cd2109728b4fee4e072b4ffb858
Andreas Zwinkau <zwinkau@kit.edu>
2010-03-30 14:26:59 +0200

    Update shell.py and svn.py from makereporting versions

--- shell.py
+++ shell.py
@@ -3,20 +3,37 @@ Convenience function
 Alternative to subprocess and os.system
 """
 import subprocess
+import sys
 
-def execute(cmd, stderr=False, stdout=False):
-	"""Execute a shell command and return stderr and/or stdout data"""
-	out, err = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+def execute(cmd, env=None):
+	"""Execute a command and return stderr and/or stdout data"""
+	out, err = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.STDOUT,\
+							env=env).communicate()
 	try:
-		if stderr:
-			for line in err.split('\n'):
-				yield line
-		if stdout:
-			for line in out.split('\n'):
-				yield line
-	except Exception as e:
+		for line in out.splitlines():
+			yield line
+	except Exception, e:
 		print "'%s' -> %s" % (c, e)
 	
+def silent_shell(cmd, env=None, debug=False):
+	"""Execute a shell command"""
+	if debug:
+		print "silent_shell", cmd
+		stdout = None
+		stderr = None
+	else:
+		stdout = open("/dev/null", 'a')
+		stderr = subprocess.STDOUT
+	try:
+	    return subprocess.call(cmd, shell=True, stdout=stdout, stderr=stderr, env=env)
+	except OSError, e:
+	    print >>sys.stderr, "Execution failed:", e
+	
+def write_file(filename, content):
+	fh = open(filename, 'w')
+	fh.write(content)
+	fh.close()
+
 if __name__ == "__main__":
 	for line in execute("hostname"):
 		print line
--- svn.py
+++ svn.py
@@ -18,25 +18,29 @@ _INFO_KEYS = {
 }
 def _get_info(svn_url):
 	info = dict()
-	for line in execute("svn info "+svn_url, stdout=True):
+	for line in execute("svn info "+svn_url):
 		for start, key in _INFO_KEYS.items():
 			if line.startswith(start):
 				info[key] = line[len(start):]
 	return info
 
+def _read_status_line(line):
+	r,a,d,l = line.split("|")
+	r = int(r[1:])
+	a = a.strip()
+	d = datetime.strptime(d.strip()[:19], "%Y-%m-%d %H:%M:%S") # TODO timezone offset
+	l = int(l.strip().split(" ")[0])
+	return r,a,d,l
+
 def _get_commit(svn_url, revision):
-	diff = "\n".join(execute("svn diff -c%d %s" % (revision, svn_url), stdout=True))
-	commit = dict(diff=diff)
-	lines = execute("svn log -c%d %s" % (revision, svn_url), stdout=True)
+	diff = "\n".join(execute("svn diff -c%d %s" % (revision, svn_url)))
+	lines = execute("svn log -c%d %s" % (revision, svn_url))
 	while not lines.next().startswith("---"):
 		pass
 	status_line = lines.next()
 	assert status_line.startswith("r")
-	r,a,d,l = status_line.split("|")
-	commit['revision'] = int(r[1:])
-	commit['author'] = a.strip()
-	commit['date'] = datetime.strptime(d.strip()[:19], "%Y-%m-%d %H:%M:%S") # TODO timezone offset
-	commit['lines'] = int(l.strip().split(" ")[0])
+	r,a,d,l = _read_status_line(status_line)
+	commit = dict(diff=diff, revision=r, author=a, date=d, lines=l)
 	msg = list()
 	line = lines.next() # skip empty line
 	line = lines.next()
@@ -45,6 +49,27 @@ def _get_commit(svn_url, revision):
 		line = lines.next()
 	commit['message'] = "\n".join(msg)
 	return commit
+
+def _get_logs(svn_url):
+	BLOCK_SIZE = 30
+	r = 0
+	for line in execute("svn log -l %d %s" % (BLOCK_SIZE, svn_url)):
+		if line.startswith("r"):
+			try:
+				radl = _read_status_line(line)
+				r = radl[0]
+				yield radl
+			except ValueError:
+				pass
+	while r:
+		for line in execute("svn log -r%d:%d %s" % (r-BLOCK_SIZE, r, svn_url)):
+			if line.startswith("r"):
+				try:
+					radl = _read_status_line(line)
+					r = radl[0]
+					yield radl
+				except ValueError:
+					pass 
 	
 def htmlize_diff(diff):
 	lines = []
@@ -80,14 +105,11 @@ class SVNRepo:
 			self._commit_cache[revision] = c
 			return c
 	def get_revisions_since(self, datetime):
-		rev = self.max_revision
-		info = self.get_commit(rev)
-		revs = list()
-		while info['date'] > datetime:
-			revs.append(rev)
-			rev -= 1
-			info = self.get_commit(rev)
-		return revs
+		for r,a,d,l in _get_logs(self.url):
+			if d > datetime:
+				yield r
+			else:
+				break
 
 if __name__ == "__main__":
 	R = SVNRepo("svn+ssh://zwinkau@ssh.info.uni-karlsruhe.de/ben/firm/svn")
