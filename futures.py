c414554daf802d0250f915c14385364a35831a5c
Andreas Zwinkau <beza1e1@web.de>
2010-03-31 07:53:18 +0200

    Adding thread handling for futures

--- futures.py
+++ futures.py
@@ -1,43 +1,68 @@
-from threading import Thread
+import threading
 """
 Futures are a simple way to execute stuff in parallel.
 Synchronization is left to the user, though.
 """
 
-# TODO use a thread pool for faster start up
-
-class _Future(Thread):
+class Promise:
 	def __init__(self, func):
-		Thread.__init__(self)
-		assert not hasattr(self, '__func')
-		assert not hasattr(self, 'result')
-		self.__func = func
-	def run(self):
-		self.result = self.__func()
+		self.func = func
+		self.lock = threading.Lock()
+		self.done = False
+	def force(self):
+		self.lock.acquire()
+		if not self.done:
+			self.result = self.func()
+			self.done = True
+		self.lock.release()
+		return self.result
 
-def future(func):
-	"""Returns a promise of the result of func()"""
-	t = _Future(func)
-	t.start()
+_PROMISES = list()
+
+def _do_promise():
+	while len(_PROMISES) > 0:
+		p = _PROMISES.pop(0) # empty list exception in concurrent case?
+		p.force()
+
+def add_worker(n=1):
+	"""Add a worker thread to fulfill promises"""
+	t = None
+	for i in xrange(n):
+		t = threading.Thread(target=_do_promise)
+		t.setDaemon(True)
+		t.start()
 	return t
-
-def force(future):
-	"""Gets the result of a promise"""
-	if future.is_alive():
-		future.join()
-	return future.result
+	
+_BASE_WORKER = None
+def future(func, *args, **kwargs):
+	"""Returns a promise of the result of func()"""
+	global _BASE_WORKER
+	p = Promise(lambda: func(*args, **kwargs))
+	_PROMISES.append(p)
+	if not _BASE_WORKER or not _BASE_WORKER.isAlive():
+		_BASE_WORKER = add_worker()
+	#print len(_PROMISES), _BASE_WORKER
+	return p
 
 if __name__ == "__main__":
-	sum = 0
+	import sys
+	extra_threads = 0
+	if len(sys.argv) > 1:
+		extra_threads = int(sys.argv[1])
+	S = 0
+	end = 1000
 	def add(i):
-		global sum
-		sum += i
-	futures = []
-	for i in range(100):
-		futures.append( future(lambda: add(i)) )
-	for f in futures:
-		force(f)
-	print sum
+		global S
+		S += i
+		sum(range(100000))
+	promises = []
+	for i in range(end):
+		promises.append( future(add, i) )
+	add_worker(extra_threads)
+	print "started %d extra worker threads" % extra_threads
+	for p in promises:
+		p.force()
+	print S, sum(range(end))
 	
 
 		
